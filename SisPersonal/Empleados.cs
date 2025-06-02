54b0f0dede52dfb960d94550a815f273ce446d8f
Facundet <facundotrejo2021@gmail.com>
2025-06-02 20:14:23 -0300

    creo el metodo MostrarEdad

--- SisPersonal/Empleados.cs
+++ SisPersonal/Empleados.cs
@@ -1,7 +1,29 @@
+using System;
 namespace EspacioEmpleados
+
 {
+    public enum Cargos
+        {
+            Administrativo,
+            Ingeniero,
+            Especialista,
+            Investigador
+        }
     public class Empleado
     {
-        
+        public string Nombre;
+        public string Apellido;
+        public DateTime FechaDeNacimiento;
+        public char EstadoCivil;
+        public DateTime FechaIngresoToEmpresa;
+        public double SueldoBasico;
+        public Cargos cargo;
+
+        public int MostrarEdad() //paso parametros cuando vengan de fuera de la clase
+        {
+            DateTime hoy = DateTime.Now;
+            int edad = hoy.Year - FechaDeNacimiento.Year;
+            return edad;
+        }
     }
 }
