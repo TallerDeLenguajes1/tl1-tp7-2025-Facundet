282ecad1aa79d1e82bfd690bbfe8f5c1810e690c
Facundet <facundotrejo2021@gmail.com>
2025-06-02 18:04:26 -0300

    creo archivo calculadora.cs

---
+++ ejercicio 1/Calculadora.cs
@@ -0,0 +1,48 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace EspacioCalculadora
+{
+    public class Calculadora
+    {
+        private double dato;
+
+
+        public void Sumar(double termino)
+        {
+            dato += termino;
+        }
+
+        public void Restar(double termino)
+        {
+            dato -= termino;
+        }
+
+        public void Multiplicar(double termino)
+        {
+            dato *= termino;
+        }
+
+        public void Dividir(double termino)
+        {
+            if (termino != 0)
+            {
+                dato /= termino;
+            }
+            else
+            {
+                Console.Write("No se puede dividir por cero.");
+            }
+        }
+
+        public void Limpiar()
+        {
+            dato = 0;
+        }
+    
+        public double Resultado
+        {
+            get { return dato; }
+        }
+}
+}
+
