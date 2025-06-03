fe31937c75dd82f25e7cfbb36219ebb86bfad361
Facundet <facundotrejo2021@gmail.com>
2025-06-03 00:30:01 -0300

    finish method to calc salary

--- SisPersonal/Empleados.cs
+++ SisPersonal/Empleados.cs
@@ -31,5 +31,42 @@ namespace EspacioEmpleados
             int edad = hoy.Year - FechaDeNacimiento.Year;
             return edad;
         }
+
+        public int RestForJubilation()
+        {
+            DateTime hoy = DateTime.Now;
+            int edad = MostrarEdad();
+            int rest = 65 - edad;
+            return rest;
+        }
+
+        public double CalcularSalario()
+        {
+            int antiguedad = MostrarAntiguedad();
+            double adicional = 0;
+
+            // ejercicio b) i)
+            if (antiguedad <= 20)
+            {
+                adicional = SueldoBasico * 0.01 * antiguedad;
+            }
+            else
+            {
+                adicional = SueldoBasico * 0.2;
+            }
+
+            // ii)
+            if (cargo == Cargos.Ingeniero || cargo == Cargos.Especialista)
+            {
+                adicional *= 1.50;
+            }
+
+            if (char.ToUpper(EstadoCivil) == 'C')
+            {
+                adicional += 150000;
+            }
+            double salarioTotal = SueldoBasico + adicional;
+            return salarioTotal;
+        }
     }
 }
